e2ad7e155f9b80473f91966bf910f454873f6916
Kiran Shila <me@kiranshila.com>
2024-03-13 09:19:22 -0700

    Add new HFSS data/model

--- src/tline_model.jl
+++ src/tline_model.jl
@@ -16,8 +16,8 @@ end
 interp(ys, xs) = scale(interpolate(ys, BSpline(Linear())), xs)
 
 rlgc_data = CSV.File("$(@__DIR__)/../data/2D_EM_RLGC.csv"; comment="#")
-rlgc_f_range = range(0.1e9, 5e9, 51)
-rlgc_w_range = range(0.2e-3, 20e-3, step=0.1e-3)
+rlgc_f_range = range(0.6e9, 2.1e9, 51)
+rlgc_w_range = range(0.2e-3, 15e-3, step=0.1e-3)
 data_range = (rlgc_f_range, rlgc_w_range)
 data_shape = (length(rlgc_f_range), length(rlgc_w_range))
 
@@ -25,7 +25,7 @@ data_shape = (length(rlgc_f_range), length(rlgc_w_range))
 rlgc_r = reshape(rlgc_data["r_ohm"], data_shape) .|> Float32
 rlgc_l = reshape(rlgc_data["l_uH"], data_shape) .* 1e-6 .|> Float32
 rlgc_g = reshape(rlgc_data["g_uS"], data_shape) .* 1e-6 .|> Float32
-rlgc_c = reshape(rlgc_data["c_pf"], data_shape) .* 1e-12 .|> Float32
+rlgc_c = reshape(rlgc_data["c_pF"], data_shape) .* 1e-12 .|> Float32
 
 const r_model = interp(rlgc_r, data_range)
 const l_model = interp(rlgc_l, data_range)
